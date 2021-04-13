4c6ad6d8976c08f2760215e1f38bf259288a2728
Silvana Ayala <silvana.ayala@nrel.gov>
2021-04-13 12:40:26 -0600

    update __init__.py

--- PVDegradationTools/__init__.py
+++ PVDegradationTools/__init__.py
@@ -1,4 +1,4 @@
-from PVDegradationTools.main import energyCalcs, relativeHumidity, BOLIDLeTID
+from PVDegradationTools.main import energyCalcs, relativeHumidity, BOLIDLeTID, Degradation
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
