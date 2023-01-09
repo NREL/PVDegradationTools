6835867396e9a686f6923c9fa5fad2a64f74442e
Brown <m.c.brown.co@gmail.com>
2023-01-09 15:58:56 -0700

    Update test_main.py
    updated tests for new parameter x=0.5

--- tests/test_main.py
+++ tests/test_main.py
@@ -112,14 +112,14 @@ def test_vantHoff_deg():
     vantHoff_deg = PVD.Degradation.vantHoff_deg(I_chamber=1000, poa_global=PSM['poa_global'],
                                                 temp_cell=PSM['temp_cell'], temp_chamber=60)
     print(vantHoff_deg)
-    assert vantHoff_deg == pytest.approx(8.98, abs=.02)
+    assert vantHoff_deg == pytest.approx(8.38, abs=.02)
 
 def test_iwa_vantHoff():
     # test the vantHoff equivalent weighted average irradiance
 
     irr_weighted_avg = PVD.Degradation.IwaVantHoff(poa_global=PSM['poa_global'],
                                                     temp_cell=PSM['temp_cell'])
-    assert irr_weighted_avg == pytest.approx(286.78, abs=0.5)
+    assert irr_weighted_avg == pytest.approx(232.47, abs=0.5)
 
 def test_arrhenius_deg():
     # test the arrhenius degradation acceleration factor
@@ -135,7 +135,7 @@ def test_arrhenius_deg():
                                               rh_outdoor=rh_surface, poa_global=PSM['poa_global'],
                                               temp_chamber=temp_chamber, temp_cell=PSM['temp_cell'],
                                               Ea=Ea)
-    assert arrhenius_deg == pytest.approx(14.34, abs=0.1)
+    assert arrhenius_deg == pytest.approx(12.804, abs=0.1)
 
 def test_iwa_arrhenius():
     # test arrhenius equivalent weighted average irradiance
@@ -145,7 +145,7 @@ def test_iwa_arrhenius():
     irr_weighted_avg = PVD.Degradation.IwaArrhenius(poa_global=PSM['poa_global'],
                                                   rh_outdoor=PSM['Relative Humidity'],
                                                   temp_cell=PSM['temp_cell'], Ea=Ea)
-    assert irr_weighted_avg == pytest.approx(247.28, abs=0.1)
+    assert irr_weighted_avg == pytest.approx(194.66, abs=0.1)
 
 def test_degradation():
     # test RH, Temp, Spectral Irradiance sensitive degradation
@@ -157,7 +157,7 @@ def test_degradation():
     degradation = PVD.Degradation.degradation(spectra=data['Spectra'], rh_module=data['RH'],
                                                 temp_module=data['Temperature'],
                                                 wavelengths=wavelengths)
-    assert degradation == pytest.approx(3.2526e-39, abs=0.05e-39)
+    assert degradation == pytest.approx(4.4969e-38, abs=0.02e-38)
 
 def test_solder_fatigue():
     # test solder fatique with default parameters
