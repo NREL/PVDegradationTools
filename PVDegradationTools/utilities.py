30f1cce943bf43e3616f4a6bfe4099efead3912f
Matthew Brown <mbrown2@el2.ib0.cm.hpc.nrel.gov>
2023-02-16 11:03:27 -0700

    tmy->h5 function

--- PVDegradationTools/utilities.py
+++ PVDegradationTools/utilities.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-from rex import NSRDBX
+import numpy as np
+from rex import NSRDBX, Outputs
 
 def write_gids(nsrdb_fp, region='Colorado', region_col='state', out_fn='gids'):
     """
@@ -32,9 +33,12 @@ def write_gids(nsrdb_fp, region='Colorado', region_col='state', out_fn='gids'):
     df_gids.to_csv('{}.csv'.format(out_fn), index=False)
 
 
-def convert_tmy(file_in, file_out):
+def convert_tmy(file_in, file_out='h5_from_tmy.h5'):
     '''
     Read a older TMY-like weather file and convert to h5 for use in PVD
+
+    TODO: figure out scale_facator and np.int32 for smaller file
+          add country and region
 
     Parameters:
     -----------
@@ -43,4 +47,33 @@ def convert_tmy(file_in, file_out):
     file_out : (str, path_obj)
         full file path and name of file to create.
     '''
-    pass
+    from pvlib import iotools
+
+    src_data, src_meta = iotools.tmy.read_tmy3(file_in)
+
+    save_cols = {'DNI':'dni',
+            'DHI':'dhi',
+            'GHI':'ghi',
+            'DryBulb':'air_temperature',
+            'DewPoint':'dew_point',
+            'RHum':'relative_humidity',
+            'Wspd':'wind_speed',
+            'Alb':'albedo'}
+
+    df_new = src_data[save_cols.keys()].copy()
+    df_new.columns = save_cols.values()
+    time_index = df_new.index
+
+    meta = {'latitude':[src_meta['latitude']],
+            'longitude':[src_meta['longitude']]}
+    meta = pd.DataFrame(meta)
+
+    with Outputs(file_out, 'w') as f:
+        f.meta = meta
+        f.time_index = time_index
+    
+    for col in df_new.columns:
+        Outputs.add_dataset(h5_file=file_out, dset_name=col,
+                            dset_data=df_new[col].values,
+                            attrs={'scale_factor':100},
+                            dtype=np.int64)
