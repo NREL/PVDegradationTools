06937fbf413f07281551467281bacd93b0d73b13
Matthew Brown <mbrown2@el2.ib0.cm.hpc.nrel.gov>
2023-02-16 11:59:22 -0700

    better metadata for coverted TMYs

--- PVDegradationTools/utilities.py
+++ PVDegradationTools/utilities.py
@@ -33,12 +33,85 @@ def write_gids(nsrdb_fp, region='Colorado', region_col='state', out_fn='gids'):
     df_gids.to_csv('{}.csv'.format(out_fn), index=False)
 
 
+def _get_state(id):
+    """
+    Returns the full name of a state based on two-letter state code
+    
+    Parameters:
+    -----------
+    id : (str)
+        two letter state code (example: CO, AZ, MD)
+
+    Returns:
+    -----------
+    state_name : (str)
+        full name of US state (example: Colorado, Arizona, Maryland)
+    """
+    state_dict = {'AK': 'Alaska',
+            'AL': 'Alabama',
+            'AR': 'Arkansas',
+            'AS': 'American Samoa',
+            'AZ': 'Arizona',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DC': 'District of Columbia',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'GU': 'Guam',
+            'HI': 'Hawaii',
+            'IA': 'Iowa',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'MA': 'Massachusetts',
+            'MD': 'Maryland',
+            'ME': 'Maine',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MO': 'Missouri',
+            'MP': 'Northern Mariana Islands',
+            'MS': 'Mississippi',
+            'MT': 'Montana',
+            'NA': 'National',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'NE': 'Nebraska',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NV': 'Nevada',
+            'NY': 'New York',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'PR': 'Puerto Rico',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VA': 'Virginia',
+            'VI': 'Virgin Islands',
+            'VT': 'Vermont',
+            'WA': 'Washington',
+            'WI': 'Wisconsin',
+            'WV': 'West Virginia',
+            'WY': 'Wyoming'}
+    state_name = state_dict[id]
+    return state_name
+    
 def convert_tmy(file_in, file_out='h5_from_tmy.h5'):
     '''
     Read a older TMY-like weather file and convert to h5 for use in PVD
 
     TODO: figure out scale_facator and np.int32 for smaller file
-          add country and region
 
     Parameters:
     -----------
@@ -65,7 +138,11 @@ def convert_tmy(file_in, file_out='h5_from_tmy.h5'):
     time_index = df_new.index
 
     meta = {'latitude':[src_meta['latitude']],
-            'longitude':[src_meta['longitude']]}
+            'longitude':[src_meta['longitude']],
+            'elevation':[src_meta['altitude']],
+            'timezone':[src_meta['TZ']],
+            'country':['United States'],
+            'state':[_get_state(src_meta['State'])]}
     meta = pd.DataFrame(meta)
 
     with Outputs(file_out, 'w') as f:
