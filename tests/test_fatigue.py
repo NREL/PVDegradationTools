dc62da61aed1ea4a6609d7cb2d839b1a4264c680
mcbrown042 <m.c.brown.co@gmail.com>
2023-06-12 10:01:45 -0600

    Update test_fatigue.py
    corrected old ref to main.Degradation

--- tests/test_fatigue.py
+++ tests/test_fatigue.py
@@ -1,8 +1,7 @@
 import os
-import pvdeg
 import pytest
 import pandas as pd
-from pvdeg import TEST_DATA_DIR
+from pvdeg import fatigue, TEST_DATA_DIR
 
 INPUT = pd.read_csv(os.path.join(TEST_DATA_DIR,'weather_year_pytest.csv'),
                     index_col=0, parse_dates=True)
@@ -11,6 +10,6 @@ def test_solder_fatigue():
     # test solder fatique with default parameters
     # requires PSM3 weather file
 
-    damage = pvdeg.Degradation.solder_fatigue(time_range=INPUT.index,
+    damage = fatigue.solder_fatigue(time_range=INPUT.index,
                                             temp_cell=INPUT['temp_cell'])
     assert damage == pytest.approx(14.25, abs=0.1)
