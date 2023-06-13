60186537d71ba47e770173aa7f8e75870a8ec84b
mcbrown042 <m.c.brown.co@gmail.com>
2023-06-13 10:25:14 -0600

    design + tutorials
    design.edge_seal_width can now run with only (weather_df, meta). renamed design.k to design.edge_seal_ingress_rate. updated tutorials and pytests to reflect change. Removed legacy pytest data

--- tests/test_design.py
+++ tests/test_design.py
@@ -5,17 +5,15 @@ from pvdeg import design, weather, humidity, TEST_DATA_DIR
 PSM_FILE = os.path.join(TEST_DATA_DIR,'psm3_pytest.csv')
 PSM, META = weather.read(PSM_FILE, 'psm')
 
-def test_k():
+def test_edge_seal_ingress_rate():
     # test calculation for constant k
 
     psat, avg_psat = humidity.psat(PSM['Dew Point'])
-    k = design.k(avg_psat=avg_psat)
+    k = design.edge_seal_ingress_rate(avg_psat=avg_psat)
     assert k == pytest.approx(.00096, abs=.000005)
 
 def test_edge_seal_width():
     # test for edge_seal_width
 
-    psat, avg_psat = humidity.psat(PSM['Dew Point'])
-    k =design.k(avg_psat=avg_psat)
-    edge_seal_width = design.edge_seal_width(k=k)
-    assert edge_seal_width == pytest.approx(0.449, abs=0.002)
+    edge_seal_width = design.edge_seal_width(weather_df=PSM, meta=META)
+    assert edge_seal_width == pytest.approx(0.7171, abs=0.0005)
