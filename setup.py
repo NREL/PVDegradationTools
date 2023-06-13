40bacfbaddc5f8f6ff1e5a6e084803ae703f7cf0
martin-springer <martinspringer.ms@gmail.com>
2023-06-13 09:24:41 -0600

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,133 +1,109 @@
-"""A setuptools based setup module.
+#!/usr/bin/env python
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-
-usage: pip install -e .
-
-"""
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+try:
+    from setuptools import setup
+except ImportError:
+    raise RuntimeError('setuptools is required')
 
 import versioneer
 from glob import glob
 
-here = path.abspath(path.dirname(__file__))
+DESCRIPTION = ('Pvdeg is a python library that supports the calculation of' +
+               'degradation related parameters for photovoltaic (PV) modules.')
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+DISTNAME = 'pvdeg'
+LICENSE = 'BSD-3'
+AUTHOR = 'Pvdeg Python Developers'
+AUTHOR_EMAIL = 'Michael.Kempe@nrel.gov'
+MAINTAINER_EMAIL = 'Silvana.Ovaitt@nrel.gov'
+URL = 'https://github.com/NREL/PVDegradationTools'
+
+PACKAGES = ['pvdeg']
+
+KEYWORDS = [
+    'photovoltaic',
+    'solar',
+    'degradation',
+    'analysis',
+    'performance',
+    'module',
+    'PV'
+]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering'
+]
+
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/NREL/PVDegradationTools/issues",
+    "Documentation": "https://pvdegradationtools.readthedocs.io/",
+    "Source Code": "https://github.com/NREL/PVDegradationTools",
+}
+
+PYTHON_REQUIRES = '>=3.9.0'
+
+SETUP_REQUIRES = [
+    'pytest-runner',
+]
+
+DOCS_REQUIRE = [
+    'sphinx',
+    'sphinx_rtd_theme'
+]
+
+TESTS_REQUIRE = [
+    'pytest',
+    'pytest-cov'
+]
+
+EXTRAS_REQUIRE = {
+    'doc': DOCS_REQUIRE,
+    'test': TESTS_REQUIRE,
+}
+
+EXTRAS_REQUIRE['all'] = sorted(set(sum(EXTRAS_REQUIRE.values(), [])))
 
 with open("requirements.txt") as f:
-    INSTALL_REQUIREMENTS = f.read().splitlines()
+    INSTALL_REQUIRES = f.read().splitlines()
 
-setup(
-    name='pvdeg',
-    python_requires='>3.9.0',
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    #version='0.1.8',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+DATA_FILES = [('DataLibrary', glob('DataLibrary/*'))]
 
+ENTRY_POINTS={"console_scripts": ["pvdeg=pvdeg.cli:cli"]}
 
-    description='Calculator to evaluate PV Degradation in PV modules.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+setuptools_kwargs = {
+    'zip_safe': False,
+    'scripts': [],
+    'include_package_data': True,
+    'package_dir' : {"pvdeg": "pvdeg"},
+}
 
-    # The project's main homepage.
-    url='https://github.com/NREL/PVDegradationTools',
-
-    # Author details
-    author='Derek Holsapple',
-    author_email='derekhols31@gmail.com',
-
-    # Choose your license
-    license='BSD',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Science/Research',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: BSD License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-
-    ],
-
-    # What does your project relate to?
-    keywords='photovoltaics pv relative humidity',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    #packages=find_packages(exclude=['contrib', 'docs', 'tests']) + ['data'],
-    packages = ['pvdeg'],
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #py_modules=["pvdeg"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=INSTALL_REQUIREMENTS,
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-
-    extras_require={
-        #'dev': ['check-manifest'],
-        #'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'pvdeg': ['data/722024TYA.pickle'],
-    #},
-
-    include_package_data=True,
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
-
-    data_files=[('DataLibrary', glob('DataLibrary/*'))],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
-    package_dir={"pvdeg": "pvdeg"},
-    entry_points={
-        "console_scripts": [
-            "pvdeg=pvdeg.cli:cli",
-        ]
-    },
-)
+setup(name=DISTNAME,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      python_requires=PYTHON_REQUIRES,
+      packages=PACKAGES,
+      keywords=KEYWORDS,
+      #setup_requires=SETUP_REQUIRES,
+      tests_require=TESTS_REQUIRE,
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      project_urls=PROJECT_URLS,
+      classifiers=CLASSIFIERS,
+      data_files=DATA_FILES,
+      entry_points=ENTRY_POINTS,
+      **setuptools_kwargs)
