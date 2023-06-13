4d4ad5ebd9647d2e7e339c2960ae99f878a58353
martin-springer <martinspringer.ms@gmail.com>
2023-06-13 08:21:25 -0600

    data files

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("requirements.txt") as f:
 
 setup(
     name='pvdeg',
-    python_requires='>3.8.0',
+    python_requires='>3.9.0',
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
@@ -112,6 +112,8 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     #data_files=[('my_data', ['data/data_file'])],
+
+    data_files=[('DataLibrary',['DataLibrary/*.json'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
