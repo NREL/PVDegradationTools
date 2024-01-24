24afef12c63adc6d80e78b78b83b5bba4a8848a7
MDKempe <58960264+MDKempe@users.noreply.github.com>
2024-01-23 17:47:23 -0700

    Update test_standards.py
    It now uses the desired power factor for wind speed height adjustment.

--- tests/test_standards.py
+++ tests/test_standards.py
@@ -10,7 +10,7 @@ import json
 import pytest
 import pandas as pd
 import pvdeg
-from pvdeg import TEST_DATA_DIR
+from pvdeg import TEST_DATA_DIR, DATA_DIR, TEST_DIR
 
 """
 TODO: during conversion from pkl to csv, a few fields dropped from float64 to float32. This appears
@@ -40,10 +40,11 @@ def test_standoff():
         azimuth=180,
         sky_model="isotropic",
         temp_model="sapm",
-        module_type="glass_polymer",
-        level=1,
+        conf_0="insulated_back_glass_polymer",
+        conf_inf="open_rack_glass_polymer",
+        T98=70,
         x_0=6.1,
-        wind_speed_factor=1.71,
+        wind_factor=0.33,
     )
 
     result_l2 = pvdeg.standards.standoff(
@@ -53,28 +54,32 @@ def test_standoff():
         azimuth=180,
         sky_model="isotropic",
         temp_model="sapm",
-        module_type="glass_polymer",
-        level=2,
+        conf_0="insulated_back_glass_polymer",
+        conf_inf="open_rack_glass_polymer",
+        T98=80,
         x_0=6.1,
-        wind_speed_factor=1.71,
+        wind_factor=0.33,
     )
 
     expected_result_l1 = {
-        "x": 2.3835484140461736,
-        "T98_0": 79.03006155479213,
-        "T98_inf": 51.11191792458173,
+        "x": 2.7381790837131876,
+        "T98_0": 79.074638,
+        "T98_inf": 53.982905,
+        "T98": 70,
     }
 
     df_expected_result_l1 = pd.DataFrame.from_dict(expected_result_l1, orient="index").T
 
     expected_result_l2 = {
-        "x": -0.20832926385165268,
-        "T98_0": 79.03006155479213,
-        "T98_inf": 51.11191792458173,
+        "x": 0,
+        "T98_0": 79.074638,
+        "T98_inf": 53.982905,
+        "T98": 80,
     }
 
     df_expected_result_l2 = pd.DataFrame.from_dict(expected_result_l2, orient="index").T
-
+    print (result_l1)
+    print (result_l2)
     pd.testing.assert_frame_equal(result_l1, df_expected_result_l1)
     pd.testing.assert_frame_equal(result_l2, df_expected_result_l2)
 
