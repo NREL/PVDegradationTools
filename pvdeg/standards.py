52942345b675891e85ffed6ae7a3fc8bc3dd8955
Silvana Ovaitt <silvana.ayala@nrel.gov>
2024-06-27 08:32:01 -0600

    albedo hack

--- pvdeg/standards.py
+++ pvdeg/standards.py
@@ -658,6 +658,9 @@ def vertical_POA(
     if meta.get('tz') == None: 
         meta['tz'] = '+0'
 
+    if "albedo" not in weather_df.columns:
+        weather_df['albedo'] = 0.2
+    
     data = {'dn':list(weather_df.dni),
            'df':list(weather_df.dhi),
             'gh':list(weather_df.ghi),
