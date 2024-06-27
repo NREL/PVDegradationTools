9035a5b2ea9c65635095fefccc5502f1b4cbf828
Silvana Ovaitt <silvana.ayala@nrel.gov>
2024-06-27 09:53:41 -0600

    different way of adding meta

--- pvdeg/standards.py
+++ pvdeg/standards.py
@@ -660,10 +660,11 @@ def vertical_POA(
 
     pv4.unassign('solar_resource_file')
                 
-    print("Type meta ", type(meta))
+    #print("Type meta ", meta)
 
     if meta.get('tz') == None: 
-        meta.loc['tz'] = '+0'
+        #meta.loc['tz'] = '+0'
+        meta = meta.append({'tz': '+0'})
 
     if "albedo" not in weather_df.columns:
         #weather_df['albedo'] = 0.2 # new pandas cries with this. dumb.
