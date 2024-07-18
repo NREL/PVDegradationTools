e7a427fca5b33b60290ed0c38f86097ba8b7f893
tobin-ford <tford@nrel.gov>
2024-07-18 15:19:04 -0600

    Diffusion: 2D, interactive plot

--- pvdeg/humidity.py
+++ pvdeg/humidity.py
@@ -1135,6 +1135,75 @@ def rh_at_sample_temperature(
 
     return rh
 
+def _calc_diff_substeps(water_new, water_old, n_steps, t, delta_t, dis, delta_dis, Fo)->None: # inplace
+    water_copy = water_old.copy()
+
+    for _ in range(n_steps): 
+        for y in range(1, water_new.shape[0] - 1):
+            # update the edge
+            water_copy[0,:] = dis
+            water_new[0,:] = dis + delta_dis # one further, do we want this?
+            
+            water_new[-1,-1] = Fo * (water_copy[-2,-1] * 4) + (1 - 4 * Fo) * water_copy[-1,-1] # inner node
+            
+            water_new[y,-1] = Fo * (water_copy[y+1,-1] + water_copy[y-1,-1] + 2 * water_copy[y,-2]) + (1 - 4 * Fo) * water_copy[y,-1] # internal edge
+
+            for x in range(y+1, water_copy.shape[1] - 1): 
+                neighbor_sum = (
+                    water_copy[y-1, x] +
+                    water_copy[y+1, x] +
+                    water_copy[y, x-1] +
+                    water_copy[y, x+1]
+                )
+                water_new[y,x] = Fo * neighbor_sum + (1 - 4 * Fo) * water_copy[y, x] # central nodes
+
+            water_new[y,y] = Fo * (2 * water_copy[y-1, y] + 2 * water_copy[y, y+1]) + (1 - 4 * Fo) * water_copy[y,y] # diagonal
+
+            dis += delta_dis
+            t += delta_t # do we need this one, temperature is not used anywhere?
+
+            water_copy = water_new.copy() # update copy so we can continue updating it
+
+def module_front(
+    chamber_properties: pd.DataFrame,
+    p = 0.1, # [cm] perimiter area
+    CW = 15.6, # [cm] cell dimensions
+    nodes = 20, # number of nodes on each axis, square cell only
+    eva_diffusivity_ea = 0.395292897, # eV
+    Dif = 2.31097881676966, # cm^2/s diffusivity prefactor
+    n_steps = 20, # number of substeps
+    )-> np.ndarray:
+
+    EaD = eva_diffusivity_ea / 0.0000861733241 # k in [eV/K]
+    W = ((CW + 2 * p) / 2) / nodes # 
+
+    # two options, we can have a 3d array that stores all timesteps
+    results = np.zeros((chamber_properties.shape[0] + 1, nodes, nodes))
+    results[0, 0, :] = chamber_properties['backsheet_moisture'].iloc[0]
+
+    for i in range(chamber_properties.shape[0] - 1): # loop over each entry the the results 
+
+        Temperature = chamber_properties['sample_temperature'].iloc[i]
+        DTemperature = chamber_properties['sample_temperature'].iloc[i+1] - chamber_properties['sample_temperature'].iloc[i] 
+        Disolved = chamber_properties['backsheet_moisture'].iloc[i]
+        DDisolved = chamber_properties['backsheet_moisture'].iloc[i+1] - chamber_properties['backsheet_moisture'].iloc[i]
+
+        time_step = (chamber_properties.index.values[i+1] - chamber_properties.index.values[i]).astype('timedelta64[s]').astype(int) # timestep in units of seconds
+        Fo = Dif * np.exp(-EaD / (273.15 + Temperature)) * time_step / (W * W)
+
+        _calc_diff_substeps(
+            water_new=results[i+1,:,:],
+            water_old=results[i,:,:],
+            n_steps=n_steps,
+            t=Temperature,
+            delta_t=DTemperature,
+            dis=Disolved,
+            delta_dis=DDisolved,
+            Fo=Fo,
+        )
+
+    return results
+
 
 # def run_module(
 #     project_points,
