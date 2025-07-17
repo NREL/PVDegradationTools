987326b3b9246430abd7e51f0447c392adb25f70
RDaxini <Rajiv.Daxini@nrel.gov>
2025-07-17 10:49:56 -0600

    single_material_dict test

--- tests/test_newmaterials_DRAFT.py
+++ tests/test_newmaterials_DRAFT.py
@@ -4,27 +4,41 @@
 """DRAFT TESTS. Not for publication"""
 
 import pytest
-from unittest.mock import patch
 from pvdeg.scenario import Scenario
 from pvdeg import utilities
 
-@patch('pvdeg.utilities.read_material')
-def test_addModule_string_material_valid(mock_read_material):
-    # Mock the read_material function to return expected data
-    mock_read_material.return_value = {
+def test_addModule_string_material_valid():
+    scenario = Scenario(name="test_scenario")
+    scenario.addModule(
+        module_name="test_module",
+        materials="OX003",
+        material_file="O2permeation",
+        parameters=['Ead', 'Do', 'Eas', 'So', 'Eap', 'Po']
+    )
+
+    assert len(scenario.modules) == 1
+    assert scenario.modules[0]["module_name"] == "test_module"
+    assert scenario.modules[0]["material_params"] == {
         'Ead': 29.43112031,
         'Do': 0.129061678,
         'Eas': 16.6314948252219,
         'So': 0.136034525059804,
         'Eap': 49.1083457348515,
         'Po': 528718258.338532
+        }
+
+def test_addModule_dict_single_material_valid():
+    materials_dict = {
+        "encapsulant": {
+            "material_file": "O2permeation",
+            "material_name": "OX003"
+        }
     }
-    
+
     scenario = Scenario(name="test_scenario")
     scenario.addModule(
         module_name="test_module",
-        materials="OX003",
-        material_file="O2permeation",
+        materials=materials_dict,
         parameters=['Ead', 'Do', 'Eas', 'So', 'Eap', 'Po']
     )
 
