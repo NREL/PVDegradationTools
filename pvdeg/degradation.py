14c08040a7de3fde3d29e8d6a83831189145b11d
RDaxini <Rajiv.Daxini@nrel.gov>
2025-08-01 09:15:59 -0600

    Update degradation.py

--- pvdeg/degradation.py
+++ pvdeg/degradation.py
@@ -38,9 +38,7 @@ def vantHoff_deg(
     Calculate Van't Hoff Irradiance Degradation acceleration factor.
 
     In this calculation, the rate of degradation kinetics is calculated using
-    the Fischer model. This degradation kinetics model was developed by interpolating 50
-    coatings with respect to color shift, cracking, gloss loss, fluorescence loss,
-    retroreflectance loss, adhesive transfer, and shrinkage.
+    the Van't Hoff model.
 
     Parameters
     ----------
