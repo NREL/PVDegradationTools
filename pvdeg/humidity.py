b230dcdb0ea80c9663476ee6a2db0ad3f85e5c89
martin-springer <martinspringer.ms@gmail.com>
2025-08-11 16:46:38 -0400

    bring back Ce_numba for working tests

--- pvdeg/humidity.py
+++ pvdeg/humidity.py
@@ -564,6 +564,101 @@ def _Ce(
     return Ce
 
 
+@jit
+def Ce_numba(
+    start,
+    temp_module,
+    rh_surface,
+    WVTRo=7970633554,
+    EaWVTR=55.0255,
+    So=1.81390702,
+    l=0.5,
+    Eas=16.729,
+):
+    """Return water concentration in encapsulant.
+
+    Calculation is used in determining Relative Humidity of Backside Solar Module
+    Encapsulant. This function returns a numpy array of the Concentration of water in
+    the encapsulant at every time step.
+
+    Numba was used to isolate recursion requiring a for loop
+    Numba Functions compile and run in machine code but can not use pandas (Very fast).
+
+    Parameters
+    -----------
+    start : float
+        Initial value of the Concentration of water in the encapsulant
+        currently takes the first value produced from
+        the _ceq(Saturation of Water Concentration) as a point
+        of acceptable equilibrium
+    temp_module : pandas series (float)
+        The surface temperature in Celsius of the solar panel module
+        "module temperature [°C]"
+    rh_Surface : list (float)
+        The relative humidity of the surface of a solar module [%]
+        EXAMPLE: "50 = 50% NOT .5 = 50%"
+    WVTRo : float
+        Water Vapor Transfer Rate prefactor [g/m2/day].
+        The suggested value for EVA is WVTRo = 7970633554(g/m2/day).
+    EaWVTR : float
+        Water Vapor Transfer Rate activation energy [kJ/mol] .
+        It is suggested to use 0.15[mm] thick PET as a default
+        for the backsheet and set EaWVTR=55.0255[kJ/mol]
+    So : float
+        Encapsulant solubility prefactor in [g/cm3]
+        So = 1.81390702(g/cm3) is the suggested value for EVA.
+    l : float
+        Thickness of the backside encapsulant [mm].
+        The suggested value for encapsulat is EVA l=0.5(mm)
+    Eas : float
+        Encapsulant solubility activation energy in [kJ/mol]
+        Eas = 16.729[kJ/mol] is the suggested value for EVA.
+
+    Returns
+    --------
+    Ce_list : numpy array
+        Concentration of water in the encapsulant at every time step
+    """
+    dataPoints = len(temp_module)
+    Ce_list = np.zeros(dataPoints)
+
+    for i in range(0, len(rh_surface)):
+        if i == 0:
+            # Ce = Initial start of concentration of water
+            Ce = start
+        else:
+            Ce = Ce_list[i - 1]
+
+        Ce = Ce + (
+            (
+                WVTRo
+                / 100
+                / 100
+                / 24
+                * np.exp(
+                    -((EaWVTR) / (0.00831446261815324 * (temp_module[i] + 273.15)))
+                )
+            )
+            / (
+                So
+                * l
+                / 10
+                * np.exp(-((Eas) / (0.00831446261815324 * (temp_module[i] + 273.15))))
+            )
+            * (
+                rh_surface[i]
+                / 100
+                * So
+                * np.exp(-((Eas) / (0.00831446261815324 * (temp_module[i] + 273.15))))
+                - Ce
+            )
+        )
+
+        Ce_list[i] = Ce
+
+    return Ce_list
+
+
 def back_encap(  # Martin: I brough this function back - Should it be deprecated? The CE calculation is currently  not working.
     rh_ambient,
     temp_ambient,
@@ -623,7 +718,7 @@ def back_encap(  # Martin: I brough this function back - Should it be deprecated
     # Need to convert these series to numpy arrays for numba function
     temp_module_numba = temp_module.to_numpy()
     rh_surface_numba = rh_surface.to_numpy()
-    Ce_nparray = _Ce(
+    Ce_nparray = Ce_numba(
         start=start,
         temp_module=temp_module_numba,
         rh_surface=rh_surface_numba,
