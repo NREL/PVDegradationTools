29d6b46417374aea46525167ecd1b900b92533ba
Kempe <mkempe@nrel.gov>
2025-08-27 14:55:13 -0600

    Updated function calls in Juypter notebooks.
    Fixed the references to humidity.py in 6 different juypter notebooks.

--- tutorials_and_tools/tutorials_and_tools/1 - Basics, Humidity, Design.py
+++ tutorials_and_tools/tutorials_and_tools/1 - Basics, Humidity, Design.py
@@ -216,7 +216,7 @@ rh_front_encap = pvdeg.humidity.front_encap(
     temp_module=temp_mod,
 )
 
-rh_back_encap = pvdeg.humidity.back_encap(
+rh_back_encap = pvdeg.humidity.Ce(
     rh_ambient=rh_surface_outside,
     temp_ambient=WEATHER["temp_air"],
     temp_module=temp_mod,
