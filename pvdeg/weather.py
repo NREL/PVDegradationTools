b5ebdb5a220a420edc9bff64ef26084f0d495012
RDaxini <Rajiv.Daxini@nrel.gov>
2025-09-04 13:46:06 -0600

    linting

--- pvdeg/weather.py
+++ pvdeg/weather.py
@@ -237,8 +237,8 @@ def get(
             temp_air = weather_df["temp_air"]
             dew_point = weather_df.get("dew_point")
             if dew_point or temp_air is None:
-                raise ValueError('Cannot calculate "relative_humidity": "dew_point" ' \
-                'column not found in DataFrame.')
+                raise ValueError('Cannot calculate "relative_humidity": one of'
+                '"dew_point" or "temp_air" column not found in DataFrame.')
             numerator = np.exp(17.625 * dew_point / (243.04 + dew_point))
             denominator = np.exp(17.625 * temp_air / (243.04 + temp_air))
             weather_df["relative_humidity"] = 100 * numerator / denominator
