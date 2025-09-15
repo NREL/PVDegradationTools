bfc74f9d8b1d8f5047913303afbe55f49e92ebe0
RDaxini <Rajiv.Daxini@nrel.gov>
2025-09-15 11:40:13 -0600

    testbook file name, update injection point

--- notebooks_testbook.py
+++ notebooks_testbook.py
@@ -10,6 +10,7 @@ import pvdeg
 import os
 
 def monkeypatch_addLocation(self, *args, **kwargs):
+    print("Monkeypatch applied!")  # Debug print
     self.gids, self.weather_data, self.meta_data = None, None, None
     GEO_META = pd.read_csv(
         os.path.join(pvdeg.TEST_DATA_DIR, "summit-meta.csv"), index_col=0
@@ -25,13 +26,23 @@ pvdeg.GeospatialScenario.addLocation = monkeypatch_addLocation
 """
 
 def monkeypatch_cells(tb):
-    # Inject the monkeypatch code at the start of the notebook
-    tb.inject(cell=0, code=monkeypatch_addLocation_code())
+    # Find a cell that contains pvdeg import and inject after it
+    for i, cell in enumerate(tb.cells):
+        if 'import pvdeg' in str(cell.source):
+            # Inject after the import cell
+            tb.inject(monkeypatch_addLocation_code(), i+1)
+            print(f"Monkeypatch injected after cell {i}")
+            break
+    else:
+        # Fallback: inject at the beginning if no pvdeg import found
+        tb.inject(monkeypatch_addLocation_code(), 0)
+        print("Monkeypatch injected at beginning")
 
 def main(notebook_path):
-    with testbook(notebook_path, execute=True) as tb:
+    with testbook(notebook_path, execute=False) as tb:
         monkeypatch_cells(tb)
-        # Optionally, add assertions to check outputs
+        # Now execute all cells
+        tb.execute()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
